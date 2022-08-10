7bdd8815b558012cbefac76cc6bf1ea488965936
Frederick Holland <f.holland124@gmail.com>
2022-08-10 17:45:18 +1000

    Added Stripe integration

--- application.py
+++ application.py
@@ -1,6 +1,39 @@
-from flask import Flask
+import json
+import os
+from tkinter import E
+import stripe
+
+from flask import Flask, jsonify, request
+
+endpoint_secret = ""
+
 application = Flask(__name__)
 
 @application.route("/")
-def hello_world():
-    return "Sup."
+def webhook():
+    event = None
+    payload = request.data
+    sig_header = request.headers["STRIPE_SIGNATURE"]
+
+    try:
+        event = stripe.Webhook.construct_event(
+            payload, sig_header, endpoint_secret
+        )
+    except ValueError as e:
+        raise e
+    except stripe.error.SignatureVerificationError as e:
+        raise e
+    
+    if event['type'] == 'checkout.session.async_payment_failed':
+        session = event['data']['object']
+    elif event['type'] == 'checkout.session.async_payment_succeeded':
+        session = event['data']['object']
+    elif event['type'] == 'checkout.session.completed':
+        session = event['data']['object']
+    elif event['type'] == 'checkout.session.expired':
+        session = event['data']['object']
+    else:
+      print('Unhandled event type {}'.format(event['type']))
+    
+    return jsonify(success=True)
+
